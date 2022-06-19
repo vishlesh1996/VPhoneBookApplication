6f3977b91d9ac8bc021837fcea7dd2b238d9050b
vishlesh11996 <vishlesh.reakalwar@gmail.com>
2022-06-19 17:13:08 +0530

    VPHON-2:create spring boot project with required dependdencies

---
+++ src/main/java/com/vish/phonebookappli/PhoneBookAppicationApplication.java
@@ -0,0 +1,13 @@
+package com.vish.phonebookappli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookAppicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookAppicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vish/phonebookappli/PhoneBookAppicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vish.phonebookappli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookAppicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
